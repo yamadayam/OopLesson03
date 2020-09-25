aa5464f720a68ccd390da8f389d24f2f6d42a5ed
修平 山田 <31128@ojs.ac.jp>
2020-09-25 15:10:59 +0900

    リンクの機能　遅延・即時実行

---
+++ Chapter3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter3 {
+    class Program {
+        //デリゲートの宣言(int型の因数を受け取ってbool値を返すメソッド)
+        public delegate bool Judgement(int value);
+
+        static void Main(string[] args) {
+            var list = new List<string>()
+            {
+                "Tokyo","New Delhe","Bangkok","London","Paris","Berlin","Canberra","Hong Kong",
+            };
+
+            //list.ForEach(s => Console.WriteLine(s));
+
+            list.ConvertAll(s => s.ToUpper()).ForEach(s => Console.WriteLine(s));                     
+
+        }
+    }
+}

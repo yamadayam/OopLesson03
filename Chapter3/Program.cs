14f6f31dbd3badf763a16d6e38d1871787544260
修平 山田 <31128@ojs.ac.jp>
2020-09-25 15:40:31 +0900

    問題3-1-2

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -15,9 +15,11 @@ namespace Chapter3 {
             {
                 12,87,94,14,53,20,40,35,76,91,31,17,48,
             };
-            var exists = numbers.Exists(s => s % 8 == 0 || s % 9 == 0);
-            Console.WriteLine(exists);
+            
 
+            numbers.ForEach(s => Console.WriteLine(s/2));
+
+            
         }
     }
 }

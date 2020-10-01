64c1261a68ecc97a852ae57e993aff3907c04aa3
修平 山田 <31128@ojs.ac.jp>
2020-10-01 10:38:08 +0900

    null条件演算子

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            string code = "12345";
+            Console.WriteLine(GetPorduct());
+        }
+        private static string GetPorduct() {
+            Sale sale = new Sale
+            {
+                ShopName = "pet store",
+                Amount = 100000,
+                Product = "food"
+            };
+            sale = null;
+            return sale?.Product;
+        }
 
-            var message = GetMessage(code) ?? DefaltMessage();
-
-            Console.WriteLine(message);
-        }
-        //スタブ
-        private static object DefaltMessage() {
-            return "DefaltMessage";
-        }
-        //スタブ
-        private static object GetMessage(string code) {
-            return "GetMessage";
-        }
+    }
+    class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //売上高
+        public int Amount { get; set; }
+        public string Product { get; set; }
     }
 }

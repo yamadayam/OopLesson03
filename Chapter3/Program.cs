c1917590188f6003386661a14aa8978dad864f40
修平 山田 <31128@ojs.ac.jp>
2020-10-01 10:21:56 +0900

    null合体演算子

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -17,20 +17,37 @@ namespace Chapter3 {
                 "Tokyo","New Delhe","Bangkok","London","Paris","Berlin","Canberra","Hong Kong",
             };
             //1
-            var line = Console.ReadLine();
-            var index = names.FindIndex(s=>s==line);
-            Console.WriteLine(index);
+            //var line = Console.ReadLine();
+            //var index = names.FindIndex(s=>s==line);
+            //Console.WriteLine(index);
+            Console.WriteLine("\n--------3.2.1--------");
+            Console.WriteLine("都市名を入力。空行で終了。");
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
+
             //2
+            Console.WriteLine("\n--------3.2.2--------");
             var value = names.Count(s => s.Contains('o'));
             Console.WriteLine(value);
+
             //3
+            Console.WriteLine("\n--------3.2.3--------");
             var whe = names.Where(s => s.Contains('o')).ToArray();
             foreach (var item in whe)
             {
                 Console.WriteLine(item);
             }
             //4
-            var sel = names.Where(s => s.Contains('B')).Select(s=>s.Length);
+            Console.WriteLine("\n--------3.2.4--------");
+            var sel = names.Where(s => s.StartsWith("B")).Select(s=>s.Length);
             foreach (var item in sel)
             {
                 Console.WriteLine(item);
---
+++ Chapter4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4 {
+    class Program {
+        static void Main(string[] args) {
+            string code = "12345";
+
+            var message = GetMessage(code) ?? DefaltMessage();
+
+            Console.WriteLine(message);
+        }
+        //スタブ
+        private static object DefaltMessage() {
+            return "DefaltMessage";
+        }
+        //スタブ
+        private static object GetMessage(string code) {
+            return "GetMessage";
+        }
+    }
+}

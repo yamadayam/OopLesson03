6061224a11ebf36e0333120401ced55bca185600
修平 山田 <31128@ojs.ac.jp>
2020-10-02 15:07:18 +0900

    問題4

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -3,30 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter4 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine(GetPorduct());
-        }
-        private static string GetPorduct() {
-            Sale sale = new Sale
+            //4.2.1
+            YearMonth[] ymCollection = new YearMonth[5] 
+            {new YearMonth(2000,12), new YearMonth(2020,12) , new YearMonth(1991,12) ,
+             new YearMonth(1656,12) , new YearMonth(2008,12) };
+
+            //4.2.2
+            Console.WriteLine("------4.2.2------");
+            foreach (var ym in ymCollection)
             {
-                ShopName = "pet store",
-                Amount = 100000,
-                Product = "food"
-            };
-            sale = null;
-            return sale?.Product;
+                Console.WriteLine(ym);
+            }
+            //4.2.4
+            Console.WriteLine("------4.2.4------");
+            if (Is21find(ymCollection)==null)
+            {
+                Console.WriteLine("21世紀のデータはありません");
+            } else
+            {
+                Console.WriteLine(Is21find(ymCollection));
+            }
+            //4.2.5
+            Console.WriteLine("------4.2.5------");
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in ymCollection)
+            {
+                Console.WriteLine(array);
+            }
+        }
+        //4.2.3
+        static YearMonth Is21find(YearMonth[] yms) {
+            foreach (var ym in yms)
+            {
+                if (ym.Is21Century)
+                {
+                    return ym;
+                }
+            }
+            return null;
         }
 
-    }
-    class Sale {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amount { get; set; }
-        public string Product { get; set; }
     }
 }
---
+++ Chapter4/YearMonth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4 {
+    class YearMonth {
+        //4.1.1
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+        //4.1.3
+        public YearMonth AddOneMonth() {
+            if (Month==12)
+            {
+                return new YearMonth(Year + 1,1);
+            } else
+            {
+                return new YearMonth(Year, Month + 1);
+            }
+        }
+
+        //4.1.4
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+       
+    }
+    
+
+    
+}

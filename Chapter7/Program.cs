931264bd8b09623836c2de89b64f96f05b7512c1
修平 山田 <31128@ojs.ac.jp>
2020-10-16 14:36:49 +0900

    演習7.1

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -10,47 +10,26 @@ using System.Threading.Tasks;
 namespace Chapter7 {
     class Program {
         static void Main(string[] args) {
-            var dict = new Dictionary<string, List<string>>() {
-               { "PC", new List<string> { } },
-               { "CD", new List<string> { } },
-            };
-
-            int host = 1;
-            while (host==1) {
-                Console.WriteLine("1．登録　2．内容を表示　3.終了");
-                Console.Write(">");
-                int reg = int.Parse(Console.ReadLine());
-                Console.WriteLine();//改行
-
-                switch (reg) {
-                    case 1:
-                        Console.Write("KEYを入力：");
-                        string key = Console.ReadLine();
-
-                        Console.Write("VALUEを入力：");
-                        string value = Console.ReadLine();
-
-                        if (dict.ContainsKey(key)) {
-                            dict[key].Add(value);
-                        } else {
-                            dict[key] = new List<string> { value };
-                        }
-                        Console.WriteLine("登録しました！");
-                        Console.WriteLine();//改行
-                        break;
-                    case 2:
-                        foreach (var item in dict) {
-                            foreach (var term in item.Value) {
-                                Console.WriteLine("{0} : {1}", item.Key, term);
-                            }
-                        }
-                        host = 2;
-                        break;
-                    case 3:
-                        Environment.Exit(0);
-                        break;
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            text.ToArray();
+            Exercise1_1(text);
+            
+        }
+        static void Exercise1_1(string text) {            
+            var dict = new Dictionary<char, int>();
+            
+            foreach (var item in text.ToUpper()) {                
+                if ('A' <= item && item <= 'Z') {
+                    if (dict.ContainsKey(item)) {
+                        dict[item]++;
+                    } else {
+                        dict[item] = 1;
+                    }
                 }
-                
+            }
+
+            foreach (var item in dict.OrderBy(s => s.Key)) {
+                Console.WriteLine("{0}：{1}", item.Key, item.Value);
             }
 
         }

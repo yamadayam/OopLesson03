2487695906c99b894e7bd275b7ca5cfc874fe1d1
修平 山田 <31128@ojs.ac.jp>
2020-11-05 09:36:22 +0900

    設定画面追加

--- SendMailApp/MainWindow.xaml.cs
+++ SendMailApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,16 +20,47 @@ namespace SendMailApp {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        SmtpClient sc = new SmtpClient();
+
         public MainWindow() {
             InitializeComponent();
+            sc.SendCompleted += Sc_SendCompleted;
         }
+
+        //送信完了イベント
+        private void Sc_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e) {
+            if (e.Cancelled) {
+                MessageBox.Show("送信キャンセルされました。");
+            } else {
+                MessageBox.Show(e.Error?.Message ?? "送信完了");
+            }
+        }
+
         //メール送信処理
         private void btOk_Click(object sender, RoutedEventArgs e) {
+            try {
+                MailMessage msg = new MailMessage("ojsinfosys01@gmail.com", tbTo.Text);
 
+                msg.Subject = tbTitle.Text;//件名
+                msg.Body = tbBody.Text;//本文
+                
+                sc.Host = "smtp.gmail.com";//SMTPサーバーの設定
+                sc.Port = 587;
+                sc.EnableSsl = true;
+                sc.Credentials = new NetworkCredential("ojsinfosys01@gmail.com", "ojsInfosys2020");
+
+                //sc.Send(msg);//送信
+                sc.SendMailAsync(msg);
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
-        
+        //送信キャンセル処理
         private void btCancel_Click(object sender, RoutedEventArgs e) {
-
+            sc.SendAsyncCancel();
         }
     }
 }

2b9e813ac956813fe4284c5f4f2fcfb2500c0074
修平 山田 <31128@ojs.ac.jp>
2020-11-05 11:20:41 +0900

    設定

---
+++ SendMailApp/Config.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMailApp {
+    class Config {
+        public string Smtp { get; set; }//SMTPサーバ
+        public string MailAddress{ get; set; }//自メールアドレス
+        public string PassWord { get; set; }//パスワード
+        public int Port{ get; set; }    //ポート番号
+        public bool Ssl { get; set; }   //SSL設定
+
+        //初期設定用
+        public void DefaultSet() {
+            Smtp = "smtp.gmail.com";
+            MailAddress = "ojsinfosys01@gmail.com";
+            PassWord = "ojsInfosys2020";
+            Port = 587;
+            Ssl = true;
+        }
+
+    }
+}
